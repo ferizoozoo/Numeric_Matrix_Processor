1900073af6e2fc8c897e4b57b657ec822a685174
ferizoozoo <farhadzohoorz@gmail.com>
2020-05-20 22:26:27 +0430

    Add TransposeMatrix class for transpose the matrix

--- matrix_processor.py
+++ matrix_processor.py
@@ -45,9 +45,58 @@ def print_matrix(mat, n, m):
         print()
 
 
+class TransposeMatrix:
+    def __init__(self):
+        print('1. Main diagonal\n2. Side diagonal\n3. Vertical line\n4. Horizontal line')
+        self.choice = int(input())
+        self.n, self.m = list(map(int, input().split()))
+        self.new_matrix = [[0 for _i in range(self.m)] for _i in range(self.n)]
+        self.matrix = read_matrix(self.n, self.m)
+        self.menu()
+
+
+    def menu(self):
+        if self.choice == 1:
+            self.main_diagonal()
+        elif self.choice == 2:
+            self.side_diagonal()
+        elif self.choice == 3:
+            self.vertical_line()
+        elif self.choice == 4:
+            self.horizontal_line()
+
+
+    def main_diagonal(self):
+        for row in range(self.n):
+            for column in range(self.m):
+                self.new_matrix[row][column] = self.matrix[column][row]
+
+
+    def side_diagonal(self):
+        for row in range(self.n):
+            for column in range(self.m):
+                self.new_matrix[row][column] = self.matrix[self.n - 1 - column][self.m - 1 - row]
+
+
+    def vertical_line(self):
+        for row in range(self.n):
+            for column in range(self.m):
+                 self.new_matrix[row][column] = self.matrix[row][self.m - 1 - column]
+
+
+    def horizontal_line(self):
+        for row in range(self.n):
+            for column in range(self.m):
+                self.new_matrix[row][column] = self.matrix[self.n - 1 - row][column]
+
+
+    def print_transposed_matrix(self):
+        print_matrix(self.new_matrix, self.n, self.m)
+
+
 def main():
     while True:
-        print('1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit')
+        print('1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n4. Transpose matrix\n0. Exit')
         choice = int(input())
         if choice == 1:
             matrices = []
@@ -73,6 +122,10 @@ def main():
             result = matrix_multiplication(matrices[0], matrices[1])
             if len(result) > 0:
                 print_matrix(result, len(matrices[0]), len(matrices[1][0]))
+        elif choice == 4:
+            transposed = TransposeMatrix()
+            if transposed:
+                transposed.print_transposed_matrix()
         elif choice == 0:
             break
 
